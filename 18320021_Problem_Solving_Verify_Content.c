dedf8fe9103686c881e7bfb53e183e58b9a0a92b
Fannan Bachtiar <nanbi.rlthingy@gmail.com>
2022-01-30 21:10:53 +0700

    Update 18320021_Problem_Solving_Verify_Content.c

--- 18320021_Problem_Solving_Verify_Content.c
+++ 18320021_Problem_Solving_Verify_Content.c
@@ -5,24 +5,24 @@
 
 int main () {
     //KAMUS
-    char fileName [maxchar_fn];
+    char namafile [maxchar_fn];
     unsigned char val[sig_byte]; 
     FILE *filep;
     //ALGORITMA
     printf("Masukkan Nama File : "); 
-    scanf("%s", fileName); // Mendefine nama file sebagai variabel s
+    scanf("%s", namafile); // Mendefine nama file sebagai variabel namafile
     
-    filep = fopen(fileName,"r");
+    filep = fopen(namafile,"r");
     if (filep != NULL){
         fread(val, sig_byte, 1, filep);
         printf("%x %x %x %x %x\n", val[0],val[1],val[2],val[3],val[4]); // Menampilkan content file
 
         if ((val[0] == 0x25) && (val[1] == 0x50) && (val[2] == 0x44) 
             && (val[3] == 0x46) && (val[4] == 0x2d)){
-            printf("File %s merupakan PDF\n", fileName);
+            printf("File %s merupakan PDF\n", namafile);
         } // File memiliki content pdf
         else if ((val[0] == 0xFF) && (val[1] == 0xd8) && (val[2] == 0xFF)) {
-            printf("File %s merupakan JPG\n", fileName);
+            printf("File %s merupakan JPG\n", namafile);
         } // File memiliki content jpg
         else {
             printf("File tidak terdefinisi\n");
